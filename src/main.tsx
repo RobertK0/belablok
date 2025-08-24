36fb9da1d5fee23c07f088bfa128710311309e94
Robert Kaurin <robert@netgen.io>
2025-08-24 22:17:49 +0200

    set up locatorjs

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from "./App";
 import "./globals.css";
 import "./i18n";
 
+import setupLocatorUI from "@locator/runtime";
+
+if (process.env.NODE_ENV === "development") {
+  setupLocatorUI();
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
